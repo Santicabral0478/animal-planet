7de3be546a140d50599e6fa9f800efc6a687b81f
Santicabral0478 <scsantiagocabral@gmail.com>
2024-06-17 10:53:10 -0300

    update 2

--- src/components/ShowSect/index.tsx
+++ src/components/ShowSect/index.tsx
@@ -3,122 +3,66 @@ export const ShowSect = ()=>{
         <section className="section showsect" aria-label="showsect">
           <div className="container">
             <ul className="has-scrollbar">
+
+
+
               <li className="scrollbar-item">
                 <div className="showsect-card">
                   <div className="card-banner img-holder">
-                    <img src="./assets/images/showsect-1.png" width="460" height="500" loading="lazy" alt="Howard Holmes"
-                      className="img-cover"/>
+                    <img src="https://i.pinimg.com/564x/c4/b0/06/c4b0061afdce35bed341289de203882b.jpg" width="460" height="500" loading="lazy" alt="Howard Holmes"
+                      className="img-cover-card"/>
+                     <div className="gradientimage"></div>
                   </div>
                   <h3 className="h3">
                     <a href="#" className="card-title">Howard Holmes</a>
                   </h3>
                   <p className="card-subtitle">Dentist</p>
-                  <ul className="card-social-list">
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-facebook"></ion-icon> */}
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-twitter"></ion-icon> */}
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-instagram"></ion-icon> */}
-                      </a>
-                    </li>
-                  </ul>
                 </div>
               </li>
+
               <li className="scrollbar-item">
                 <div className="showsect-card">
                   <div className="card-banner img-holder">
-                    <img src="./assets/images/showsect-2.png" width="460" height="500" loading="lazy" alt="Ella Thompson"
-                      className="img-cover"/>
+                    <img src="https://i.pinimg.com/564x/c4/b0/06/c4b0061afdce35bed341289de203882b.jpg" width="460" height="500" loading="lazy" alt="Howard Holmes"
+                      className="img-cover-card"/>
+                     <div className="gradientimage"></div>
                   </div>
                   <h3 className="h3">
-                    <a href="#" className="card-title">Ella Thompson</a>
+                    <a href="#" className="card-title">Howard Holmes</a>
                   </h3>
                   <p className="card-subtitle">Dentist</p>
-                  <ul className="card-social-list">
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-facebook"></ion-icon> */}
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-twitter"></ion-icon> */}
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-instagram"></ion-icon> */}
-                      </a>
-                    </li>
-                  </ul>
                 </div>
               </li>
+
               <li className="scrollbar-item">
                 <div className="showsect-card">
                   <div className="card-banner img-holder">
-                    <img src="./assets/images/showsect-3.png" width="460" height="500" loading="lazy" alt="Vincent Cooper"
-                      className="img-cover"/>
+                    <img src="https://i.pinimg.com/564x/c4/b0/06/c4b0061afdce35bed341289de203882b.jpg" width="460" height="500" loading="lazy" alt="Howard Holmes"
+                      className="img-cover-card"/>
+                     <div className="gradientimage"></div>
                   </div>
                   <h3 className="h3">
-                    <a href="#" className="card-title">Vincent Cooper</a>
+                    <a href="#" className="card-title">Howard Holmes</a>
                   </h3>
                   <p className="card-subtitle">Dentist</p>
-                  <ul className="card-social-list">
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-facebook"></ion-icon> */}
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-twitter"></ion-icon> */}
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-instagram"></ion-icon> */}
-                      </a>
-                    </li>
-                  </ul>
                 </div>
               </li>
+
               <li className="scrollbar-item">
                 <div className="showsect-card">
-                  <div className="card-banner img-holder" >
-                    <img src="./assets/images/showsect-4.png" width="460" height="500" loading="lazy" alt="Danielle Bryant"
-                      className="img-cover"/>
+                  <div className="card-banner img-holder">
+                    <img src="https://i.pinimg.com/564x/c4/b0/06/c4b0061afdce35bed341289de203882b.jpg" width="460" height="500" loading="lazy" alt="Howard Holmes"
+                      className="img-cover-card"/>
+                     <div className="gradientimage"></div>
                   </div>
                   <h3 className="h3">
-                    <a href="#" className="card-title">Danielle Bryant</a>
+                    <a href="#" className="card-title">Howard Holmes</a>
                   </h3>
                   <p className="card-subtitle">Dentist</p>
-                  <ul className="card-social-list">
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-facebook"></ion-icon> */}
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-twitter"></ion-icon> */}
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="card-social-link">
-                        {/* <ion-icon name="logo-instagram"></ion-icon> */}
-                      </a>
-                    </li>
-                  </ul>
                 </div>
               </li>
+
+
             </ul>
           </div>
         </section>
